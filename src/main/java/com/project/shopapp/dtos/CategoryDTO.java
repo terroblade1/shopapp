e981fe080f3c7c2085c835346f09d565f02eab0f
LE THANH NGUYEN <leesin97123@gmail.com>
2025-03-01 15:47:13 +0700

    create project + create controller, dto, model: category + product + user

---
+++ src/main/java/com/project/shopapp/dtos/CategoryDTO.java
@@ -0,0 +1,15 @@
+package com.project.shopapp.dtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryDTO {
+    @NotEmpty(message = "Category's name cannot be empty")
+    private String name;
+}
